413a3d0a6b595c2e6963841e48b4bdb372280c77
ramit21 <21.ramit@gmail.com>
2021-09-10 18:10:09 +0530

    lex

--- my-chatbot-app/src/main.ts
+++ my-chatbot-app/src/main.ts
@@ -4,10 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import { Amplify } from 'aws-amplify';
-//import { amplify } from './aws-exports';
-
-//Amplify.configure(amplify);
+import Amplify from "aws-amplify";
+//@ts-ignore
+import aws_exports from "./aws-exports";
+Amplify.configure(aws_exports);
 
 if (environment.production) {
   enableProdMode();
